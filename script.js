8501d5c48c54048993395a3d2c16dc08ec631d38
AnkaBagishvili <lukiana.3@gmail.com>
2024-01-07 12:41:52 -0800

    catalog-section1/2

---
+++ script.js
@@ -0,0 +1,15 @@
+let burger = document.querySelector(".burger");
+let xmark = document.querySelector(".xmark");
+let mobileMenu = document.querySelector(".header_nav");
+
+burger.addEventListener("click", () => {
+  mobileMenu.classList.add("show");
+  burger.style.display = "none";
+  xmark.style.display = "block";
+});
+
+xmark.addEventListener("click", () => {
+  mobileMenu.classList.remove("show");
+  burger.style.display = "block";
+  xmark.style.display = "none";
+});
